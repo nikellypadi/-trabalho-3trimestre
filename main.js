e4ce5f89cd6c43037df89a91532f1341a4ebbbf4
nikellypadi <145352451+nikellypadi@users.noreply.github.com>
2023-11-27 10:38:00 -0300

    L

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const listaDeTeclas = document.querySelectorA11(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){ 
-    const efeito = listaDeTeclas[contador].classiList[1];
+    const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){ 
         tocaSom (idAudio)
