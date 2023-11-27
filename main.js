10ac6b1013f20f21c8dcf8494bc5cd9d704ce59a
nikellypadi <145352451+nikellypadi@users.noreply.github.com>
2023-11-27 10:36:42 -0300

    NK

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function tocaSom(idE1emetoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(idE1ementoAudio){
+    document.querySelector(idE1ementoAudio).play();
 } 
 const listaDeTeclas = document.querySelectorA11(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){ 
-    const efeito = listaDeTeclas[contador].classilist[1];
+    const efeito = listaDeTeclas[contador].classiList[1];
     const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function (){ 
+    listaDeTeclas[contador].onclick = function(){ 
         tocaSom (idAudio)
     }
     contador = contador + 1;
